1d5e9dc042b519224adf5bca7d65dc13ad1aff4b
Carrie Whitson <CarrieW23@users.noreply.github.com>
2020-07-02 19:24:50 -0700

    Addition of quotes

--- Random Quote Generator/js/script.js
+++ Random Quote Generator/js/script.js
@@ -3,14 +3,43 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
+var quotes = [
+  {
+  quote: '"For love casts out fear and gratitude can conquer pride."',
+  source:'Louisa May Alcott',
+  citation:'Little Women',
+  year: 1868
+},
+{
+  quote:'"A second chance doesn\'t mean you\'re in the clear. In many ways, it is the more difficult thing. Because a second chance means that you have to try harder. You must rise to the challenge without the blind optimism of ignorance."',
+  source:'Ling Ma',
+  citation:'Severance',
+  year: 2018
+},
+{
+  quote:'"When you\'re a small piece of a big puzzle, you gotta fix what you can."',
+  source:'Hushpuppy',
+  citation:'Beast of the Southern Wild',
+  year: 2012
+},
+{
+  quote: '"What I have in mind is that art may be bad, good or indifferent, but, whatever adjective is used, we must call it art, and bad art is still art in the same way that a bad emotion is still an emotion."',
+  source:'Marcel DuChamp',
+},
+{
+  quote: 'The most exciting, challenging, and significant relationship of all is the one you have with yourself.',
+  source:'Carrie Bradshaw',
+  citation:'Sex and the City',
+  year: 2004
+}
+]
 
 
 /***
